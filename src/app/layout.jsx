350c606ae94e2d73b12e7819a5dd2cc88e403be7
gomaa11 <mysiet2@gmail.com>
2025-10-05 16:00:33 +0200

    switch6

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -8,49 +8,26 @@ const baseUrl = "https://gomaa.vercel.app"; // 👈 غيّرها حسب الدو
 export const metadata = {
   title: "Eng : Gomaa Shaban",
   description: "Gomaa Shaban : Web and Android Applications Programmer",
-  icons: {
-    icon: [
-      { url: "/images/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-      { url: "/images/favicon-32x32.png", sizes: "32x32", type: "image/png" },
-      { url: "/images/favicon.ico", sizes: "48x48", type: "image/png" },
-    ],
-    apple: [
-      {
-        url: "/images/android-chrome-192x192.png",
-        sizes: "192x192",
-        type: "image/png",
-      },
-    ],
-    other: [
-      {
-        rel: "icon",
-        url: "/images/android-chrome-512x512.png",
-        sizes: "512x512",
-        type: "image/png",
-      },
-    ],
-  },
   openGraph: {
     title: "Eng : Gomaa Shaban",
     description: "Gomaa Shaban : Web and Android Applications Programmer",
-    url: baseUrl, // 👈 مهم
-    siteName: "Gomaa Shaban Portfolio",
+    url: "https://profilegomaa.vercel.app",
+    type: "website",
     images: [
       {
-        url: `${baseUrl}/og-image.png`, // 👈 هنا التعديل المهم
+        url: "https://profilegomaa.vercel.app/og-image.png",
         width: 1200,
-        height: 630, // الأفضل يكون 1200x630
+        height: 630,
         alt: "Eng : Gomaa Shaban",
+        type: "image/png",
       },
     ],
-    locale: "en_US",
-    type: "website",
   },
   twitter: {
     card: "summary_large_image",
     title: "Eng : Gomaa Shaban",
     description: "Gomaa Shaban : Web and Android Applications Programmer",
-    images: [`${baseUrl}/og-image.png`], // 👈 خليها نفس الصورة المطلقة
+    images: ["https://profilegomaa.vercel.app/og-image.png"],
   },
 };
 
